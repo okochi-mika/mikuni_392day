fb1cee46198fe4fce4ef2278ddb8cb6c438b16a9
okochi-mika <hiyoko_m@icloud.com>
2026-04-18 22:40:38 +0900

    最初のコミット

---
+++ setlist.js
@@ -0,0 +1,12 @@
+// setlist.js の冒頭
+const setlistData = [
+    { title: "1. Naked ", id: "06PuQc6pwII" }, // YouTube IDを調べて入れる
+    { title: "2. 遠い星", id: "0Cz_jmdLYrE" },
+    { title: "3. all the way", id: "TMoOdbjcjGU" }, 
+    { title: "4. If ~もしも願いが叶うなら~", id: "HEHvGgZsdl8" },
+    { title: "5. 夏花火", id: "ZjvQ6Vj_wuw" },
+    { title: "6. はじまり", id: "FZsIi1KH4xI" },
+    { title: "7. それが、愛でしょう", id: "0mTnSzDFhyE" },
+    { title: "8. Alone", id: "HKOy3b5ZSVQ" },
+    { title: "9. この場所が好きで", id: "JjWwfUbOP0w" }
+];
