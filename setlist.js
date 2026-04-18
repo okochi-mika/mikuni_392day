21a8ef199c95d7349f8ea44a8efb0cbbad1e135d
okochi-mika <hiyoko_m@icloud.com>
2026-04-18 22:52:38 +0900

    セトリを更新

--- setlist.js
+++ setlist.js
@@ -2,11 +2,11 @@
 const setlistData = [
     { title: "1. Naked ", id: "06PuQc6pwII" }, // YouTube IDを調べて入れる
     { title: "2. 遠い星", id: "0Cz_jmdLYrE" },
-    { title: "3. all the way", id: "TMoOdbjcjGU" }, 
-    { title: "4. If ~もしも願いが叶うなら~", id: "HEHvGgZsdl8" },
-    { title: "5. 夏花火", id: "ZjvQ6Vj_wuw" },
-    { title: "6. はじまり", id: "FZsIi1KH4xI" },
-    { title: "7. それが、愛でしょう", id: "0mTnSzDFhyE" },
-    { title: "8. Alone", id: "HKOy3b5ZSVQ" },
-    { title: "9. この場所が好きで", id: "JjWwfUbOP0w" }
+    { title: "3. all the way", id: "ZjvQ6Vj_wuw" }, 
+    { title: "4. If ~もしも願いが叶うなら~", id: "ef1JsUS7Qkk" },
+    { title: "5. 夏花火", id: "KKYMYE1gyNI" },
+    { title: "6. はじまり", id: "JjWwfUbOP0w" },
+    { title: "7. それが、愛でしょう", id: "TMoOdbjcjGU" },
+    { title: "8. Alone", id: "KKYMYE1gyNI" },
+    { title: "9. この場所が好きで", id: "tMR3hb4iR34" }
 ];
